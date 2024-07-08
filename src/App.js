00aa9913ea572d029797ef2c8ed47c3a9f71abf9
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2024-07-08 13:56:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-// src/App.js
+import React, { useState } from 'react';
+import { parse } from 'papaparse';
+import DataTable from './DataTable';
 
-import React from 'react';
-import CSVReader from './CSVReader';
-import { Container } from '@mui/material';
+function App() {
+  const [data, setData] = useState([]);
+  const [columns, setColumns] = useState([]);
 
-const App = () => {
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    parse(file, {
+      complete: (result) => {
+        const parsedData = result.data;
+        setColumns(Object.keys(parsedData[0]));
+        setData(parsedData);
+      },
+      header: true,
+    });
+  };
+
   return (
-    <Container>
+    <div>
       <h1>CSV Reader</h1>
-      <CSVReader />
-    </Container>
+      <input type="file" accept=".csv" onChange={handleFileUpload} />
+      {data.length > 0 && (
+        <DataTable data={data} columns={columns} />
+      )}
+    </div>
   );
-};
+}
 
 export default App;
